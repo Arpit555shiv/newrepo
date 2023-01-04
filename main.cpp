685ed01e0aea529a19cc4464b907388a410d0e4e
Arpit555shiv <121945613+Arpit555shiv@users.noreply.github.com>
2023-01-04 17:42:53 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,36 @@
 
+#include<iostream>
+using namespace std;
+
+
+const int maxn = 100005;
+
+int deg[maxn];
+
+int main()
+{
+   int ver;
+   cin >> ver;
+
+   for(int i = 0 ; i < ver - 1; i++)
+   {
+      int u , v;
+      cin >> u >> v;
+      deg[u]++;
+      deg[v]++;
+
+
+   }
+
+   for(int i = 1 ; i <= ver ; i++)
+   {
+      if(deg[i] == 2)
+      {
+         cout << "NO" << '\n';
+         return 0;
+
+      }
+   }
+
+   cout << "YES" << '\n';
+}
